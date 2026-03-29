be127a0c47574baed6f92115ac8bfbdeaeb1a655
Maximilian Kielholz <maxikie02@gmail.com>
2026-03-29 19:13:42 +0200

    WIP first profile page with dummy data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,60 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 function App() {
+  const player = {
+    region: "Europe West",
+    profileIconId: 7,
+    summonerLevel: 787,
+    gameName: "spineflipper37",
+    tagLine: "000",
+    tier: "EMERALD",
+    rank: "II",
+    leaguePoints: 85,
+    wins: 69,
+    losses: 70,
+  };
+
+  const winRate = Math.round(
+    (player.wins / (player.wins + player.losses)) * 100,
+  );
+  const rankEmblemUrl = (tier: string) =>
+    `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/ranked-emblem/emblem-${tier.toLowerCase()}.png`;
+
   return (
     <>
-      <section id="center">
-        <div>
-          <h1>CLIMB.GG</h1>
+      <main className="flex flex-col items-center gap-8 mt-20">
+        <h1 className="text-4xl font-bold">focused.gg</h1>
+        <div className="flex flex-col md:flex-row gap-6 w-full max-w-3xl">
+          <img
+            className="h-20 rounded-md"
+            src={`https://ddragon.leagueoflegends.com/cdn/16.6.1/img/profileicon/${player.profileIconId}.png`}
+          />
+          <div className="text-left">
+            <p className="text-xl font-semibold">
+              {player.gameName} #{player.tagLine}
+            </p>
+            <p className="text-md font-medium">Level {player.summonerLevel}</p>
+            <p className="font-medium">{player.region}</p>
+          </div>
         </div>
-        <div className="flex">
-          <Input />
-          <Button className="ml-1">Search</Button>
+        <div className="flex flex-row w-full">
+          <div>
+            <img
+              className="h-128"
+              src={rankEmblemUrl(player.tier)}
+              alt={player.tier}
+            />
+          </div>
+          <div className="flex flex-col">
+            <div className="text-xl font-bold">
+              {player.tier} {player.rank} {player.leaguePoints} LP
+            </div>
+            <div className="font-medium">
+              {player.wins}W / {player.losses}L - {winRate}% WR
+            </div>
+          </div>
         </div>
-      </section>
+      </main>
     </>
   );
 }
