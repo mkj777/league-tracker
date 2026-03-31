e95dad195c8cb3b28c38147fb6f9869dd2152223
Maximilian Kielholz <maxikie02@gmail.com>
2026-04-01 00:08:53 +0200

    remove coming soon screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import logo from "./assets/logo.svg";
-import comingSoon from "./assets/comingsoon.svg";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <img
-      src={logo}
-      className="absolute -translate-x-1/2 -translate-y-full top-1/2 left-1/2"
-    ></img>
-    <img
-      src={comingSoon}
-      className="absolute -translate-x-1/2 translate-y-full top-1/2 left-1/2 text-gray-600"
-    ></img>
+    <App />
   </StrictMode>,
 );
