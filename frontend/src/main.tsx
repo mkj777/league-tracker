b151031bd3c25bad6b6265cc6b2c951c34b197de
Maximilian Kielholz <maxikie02@gmail.com>
2026-03-31 23:57:49 +0200

    feat: removed the app, added coming soon screen. updated favicon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <img
+      src=".\src\assets\logo.svg"
+      className="absolute -translate-x-1/2 -translate-y-full top-1/2 left-1/2"
+    ></img>
+    <img
+      src=".\src\assets\comingsoon.svg"
+      className="absolute -translate-x-1/2 translate-y-full top-1/2 left-1/2 text-gray-600"
+    ></img>
   </StrictMode>,
 );
